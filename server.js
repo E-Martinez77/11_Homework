5ceeabdee778c1ec789f9b8e2a9aab45cd89457f
E-Martinez77 <emartizzle2185@me.com>
2021-02-28 22:07:55 -0700

    added public as static in express

--- server.js
+++ server.js
@@ -10,6 +10,15 @@ const fs = require("fs");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
+
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "./public/index.html"))
+);
+
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "./public/notes.html"))
+);
 
 app.listen(PORT, () => {
   console.log(`App listening on PORT: ${PORT}`);
