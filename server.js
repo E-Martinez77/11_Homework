3689f85506b707bcae7bf8991800452062134201
E-Martinez77 <emartizzle2185@me.com>
2021-02-28 21:47:03 -0700

    updated server

--- server.js
+++ server.js
@@ -11,18 +11,6 @@ const fs = require("fs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "../index.html"));
-});
-
-app.get("notes", function (req, res) {
-  res.sendFile(path.join(__dirname, "../notes.html"));
-});
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../index.html"));
-});
-
 app.listen(PORT, () => {
   console.log(`App listening on PORT: ${PORT}`);
 });
