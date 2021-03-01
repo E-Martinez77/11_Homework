1d43641147999b728fe066084add0d4b4d5a15c9
E-Martinez77 <emartizzle2185@me.com>
2021-02-28 21:37:04 -0700

    get routes added

--- server.js
+++ server.js
@@ -4,12 +4,24 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
+const path = require("path");
+
+const fs = require("fs");
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//All routes
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "../index.html"));
+});
 
-// require(``)(app)
+app.get("notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "../notes.html"));
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on PORT: ${PORT}`);
