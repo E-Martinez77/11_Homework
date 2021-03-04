4f73f0683a6082f74bf9338ae17f4137c6a6a6f1
E-Martinez77 <emartizzle2185@me.com>
2021-03-03 18:32:26 -0700

    Post route

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 
@@ -8,9 +9,31 @@ const path = require("path");
 
 const fs = require("fs");
 
+const data = require("./db/db.json");
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+
+app.get("/api/notes", function (req, res) {
+  const read = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  //   console.log(read);
+  res.json(read);
+});
+
+app.post("/api/notes", function (req, res) {
+  //get info from db
+  const pageContent = req.body;
+  const read2 = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  //   const last = read2.length > 0 ? read2[read2.length - 1] : null; //gets last element in database, returns null if empty
+  //   const id = last !== null ? last.id++ : 1; //uuidv4
+  const id = uuidv4();
+  const newDatabase = [...read2, { ...pageContent, id }];
+
+  fs.writeFileSync("./db/db.json", JSON.stringify(newDatabase), "utf8");
+
+  res.json(newDatabase);
+});
 
 app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "./public/index.html"))
@@ -19,6 +42,16 @@ app.get("/", (req, res) =>
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "./public/notes.html"))
 );
+
+//get route, post, and delete
+
+// app.delete("/api/notes", (req, res) => res.json(data));
+
+// app.get("./db/db.json", (req, res) => res.json(data));
+
+app.get("/data", function (req, res) {
+  res.json(data);
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on PORT: ${PORT}`);
