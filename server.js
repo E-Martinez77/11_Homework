2dd2775dc21481db51f9e75839268066309ba9fd
E-Martinez77 <emartizzle2185@me.com>
2021-04-18 10:45:44 -0600

    pushed to heroku

--- server.js
+++ server.js
@@ -17,16 +17,12 @@ app.use(express.static("public"));
 
 app.get("/api/notes", function (req, res) {
   const read = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-  //   console.log(read);
   res.json(read);
 });
 
 app.post("/api/notes", function (req, res) {
-  //get info from db
   const pageContent = req.body;
   const read2 = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-  //   const last = read2.length > 0 ? read2[read2.length - 1] : null; //gets last element in database, returns null if empty
-  //   const id = last !== null ? last.id++ : 1; //uuidv4
   const id = uuidv4();
   const newDatabase = [...read2, { ...pageContent, id }];
 
@@ -40,30 +36,15 @@ app.delete("/api/notes/:id", function (req, res) {
 
   const read3 = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
 
-  // console.log(read3);
-  // console.log(id2);
-
   for (let i = 0; i < read3.length; i++) {
     if (read3[i].id === id2) {
       read3.splice(i, 1);
     }
   }
-  // console.log(read3);
-
-  //for loop
 
   fs.writeFileSync("./db/db.json", JSON.stringify(read3), "utf8");
   res.json(read3);
-
-  //read the file, update array pulled from the file, splice/clear(), and remove the item from the array and write it back to the file.
-  //find index of what was clicked and splice it out.
-
-  // res.json(id2);
 });
-
-// app.delete('/user', function (req, res) {
-//   res.send('Got a DELETE request at /user')
-// })
 
 app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "./public/index.html"))
@@ -72,12 +53,6 @@ app.get("/", (req, res) =>
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "./public/notes.html"))
 );
-
-//get route, post, and delete
-
-// app.delete("/api/notes", (req, res) => res.json(data));
-
-// app.get("./db/db.json", (req, res) => res.json(data));
 
 app.get("/data", function (req, res) {
   res.json(data);
